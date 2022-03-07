fe92415dbb478f576f29bc8eff6377a9e9fbad8b
Adam Galloway <adam@launchcode.xyz>
2022-03-07 12:32:51 +0000

    Single post page changes

---
+++ peepl-wordpress/template-parts/content/content-single.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header alignwide">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+        <p><?php
+        printf(
+            // '<a class="author-link" href="%1$s" rel="author">%2$s</a>',
+            // esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+            sprintf(
+                /* translators: %s: Author name. */
+                esc_html__( 'By %s', 'twentytwentyone' ),
+                get_the_author()
+            )
+        );
+        ?></p>
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
+				'after'    => '</nav>',
+				/* translators: %: Page number. */
+				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer default-max-width">
+		<?php twenty_twenty_one_entry_meta_footer(); ?>
+	</footer><!-- .entry-footer -->
+
+	<?php if ( ! is_singular( 'attachment' ) ) : ?>
+		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
+	<?php endif; ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
