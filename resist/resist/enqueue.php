68d87c406d3acef4d3c805251893673d6c649c3e
Zarino Zappia <mail@zarino.co.uk>
2022-04-15 12:25:07 +0100

    [WIP] "RESIST" theme

---
+++ resist/resist/enqueue.php
@@ -0,0 +1,59 @@
+<?php
+
+function resist_enqueue_styles() {
+    wp_enqueue_style(
+        'google-fonts',
+        'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Work+Sans&display=swap',
+        array(),
+        null
+    );
+
+    wp_enqueue_style(
+        'resist-style',
+        get_stylesheet_uri(),
+        array(
+            'twenty-twenty-one-style'
+        ),
+        wp_get_theme()->get( 'Version' )
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'resist_enqueue_styles' );
+
+
+function resist_enqueue_admin_styles() {
+    wp_enqueue_style(
+        'google-fonts',
+        'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Work+Sans&display=swap',
+        array(),
+        null
+    );
+
+    wp_enqueue_style(
+        'resist-admin-style',
+        get_stylesheet_uri(),
+        array( ),
+        wp_get_theme()->get( 'Version' )
+    );
+}
+
+add_action( 'admin_enqueue_scripts', 'resist_enqueue_admin_styles' );
+
+
+function resist_enqueue_editor_styles() {
+    add_editor_style(
+        '/editor-style.css'
+    );
+}
+
+add_action( 'admin_init', 'resist_enqueue_editor_styles' );
+
+
+function resist_google_fonts_resource_hint($hints, $relation_type) {
+    if ( $relation_type === 'preconnect' ) {
+        $hints[] = 'https://fonts.gstatic.com';
+    }
+    return $hints;
+}
+
+add_filter( 'wp_resource_hints', 'resist_google_fonts_resource_hint', 10, 2 );
